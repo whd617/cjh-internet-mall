741566d6fa2217d7ddc00d1ff4d569402de4e8fa
Jonghyeon Choi <whd617@naver.com>
2023-11-20 17:45:28 +0900

    feat: DB 모델 정의

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 import express from 'express';
 import { SERVER_PORT } from './constants/app.constant.js';
-import Router from './middlewares/need-signin.middleware.js';
 import db from './models/index.cjs';
-const app = express();
 
-app.use(express.json());
-app.use('/api', Router);
+const { Products, Users } = db;
 
-try {
-  await db.sequelize.authenticate();
-  console.log(`Connection has been established successfully.`);
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+// await Users.create({ email: 'test', password: 'test', name: 'test' });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!');
+// await Products.create({
+//   userId: 1,
+//   title: 'testProduct',
+//   description: 'testProductDDD',
+//   status: 'SOLD_OUT',
+// });
+
+const products = await Products.findAll();
+const users = await Users.findAll();
+
+console.log({
+  products: products.map((product) => product.toJSON()),
+  users: users.map((user) => user.toJSON()),
 });
 
+const app = express();
+
 app.listen(SERVER_PORT, () => {
-  console.log(`📡 App Listneing on port`);
+  console.log(`Example app listening on port ${SERVER_PORT}`);
 });
--- constants/app.constant.js
+++ constants/app.constant.js
@@ -1,3 +1,2 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 export const SERVER_PORT = process.env.SERVER_PORT;
